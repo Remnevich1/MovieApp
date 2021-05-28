7f9947995bfd9e15b1275d28f6f773874ab14da5
Remnevich <remnevich@gmail.com>
2021-05-28 11:43:28 +0300

    Pagination finished

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const API_KEY = 'efc975a7-2cb3-49ac-8a5a-a9e92572bb98';
-const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=';
 const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+let searchStr = '';
 
-getMovies(API_URL_POPULAR)
+getMovies(API_URL_POPULAR);
 
-async function getMovies(url) {
+async function getMovies(url, pageId = 1) {
     const resp = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
@@ -12,7 +13,9 @@ async function getMovies(url) {
         }
     });
     const respData = await resp.json();
+    console.log(respData)
     renderMovies(respData);
+    renderPagination(respData, pageId);
 }
 
 function getClassByRate (rate) {
@@ -53,6 +56,71 @@ function renderMovies(data) {
     })
 }
 
+const paginationEl = document.querySelector('.pagination');
+
+const getPaginEl = (i, searchStr, isActive, text) => {
+    const paginEl = document.createElement('a');
+    if (searchStr) {
+        const apiSearchUrl = `${API_URL_SEARCH}${searchStr}`
+        paginEl.href = apiSearchUrl + '&page=' + (i + 1);
+    } else {
+        paginEl.href = API_URL_POPULAR + (i + 1);
+    }
+    paginEl.classList.add('paginationEl');
+    if (isActive) {
+        paginEl.classList.add('active');
+    }
+    paginEl.setAttribute('data-id', i + 1);
+    paginEl.innerHTML = text || i + 1;
+    return paginEl
+}
+
+function getDots () {
+    const dots = document.createElement('span');
+    dots.classList.add('whiteDots')
+    dots.innerHTML = '...';
+    return dots
+}
+
+function renderPagination(data, pageId) {
+    document.querySelector('.pagination').innerHTML = '';
+    if (pageId > 1) {
+        paginationEl.appendChild(getPaginEl(pageId - 2, searchStr, false, '<-'))
+    }
+    paginationEl.appendChild(getPaginEl(0, searchStr, 1 == pageId))
+    if (data.pagesCount > 2) {
+        if(1 == pageId || data.pagesCount == pageId) {
+            paginationEl.appendChild(getDots())
+        } else {
+            // первый раз показать точки?
+            if (pageId > 2) {
+                paginationEl.appendChild(getDots())
+            }
+            // показать активную страницу
+            paginationEl.appendChild(getPaginEl(pageId - 1, searchStr, true))
+            // второй раз показать точки?
+            if (pageId < data.pagesCount -1) {
+                paginationEl.appendChild(getDots())
+            }
+        }
+        // for (let i = 0; i < data.pagesCount; i++) {
+        //     const paginEl = getPaginEl(i, searchStr, i + 1 == pageId)
+        //     paginationEl.appendChild(paginEl)
+        // }
+    }
+    if(data.pagesCount != 1) {
+        paginationEl.appendChild(getPaginEl(data.pagesCount - 1, searchStr, data.pagesCount == pageId))
+    }
+    if (pageId < data.pagesCount) {
+        paginationEl.appendChild(getPaginEl(+pageId, searchStr, false, '->'))
+    }
+}
+paginationEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    const paginEls = document.querySelector('.paginationEl')
+    getMovies(e.target.href, e.target.getAttribute('data-id'))
+})
+
 const form = document.querySelector('form');
 const search = document.querySelector('.header__content-search');
 
@@ -60,6 +128,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
     if (search.value) {
+        searchStr = search.value;
         getMovies(apiSearchUrl);
         search.value = ''
     }
